dead0bf75cd3c610f056182fbd91ef26b2281867
frankyfloresse <foz.fox2015@gmail.com>
2025-02-03 01:50:00 +0200

    fix css loading

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
-import { createRoot } from 'react-dom/client';
 import './index.css';
+import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { routes } from './routes/routes.tsx';
 import { Provider } from 'react-redux';
